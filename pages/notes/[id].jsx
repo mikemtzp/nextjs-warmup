74bd84778a59ae3a1a0eb3b6cc107e359bead48b
mikemtzp <mikemp8226@gmail.com>
2022-11-23 17:48:40 -0600

    Add notes route with :id route

---
+++ pages/notes/[id].jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+// By putting our component in the route pages/notes/[id].jsx
+// Next will automatically set the rouyte to be 'http://localhost:3000/notes/:id'
+const Page = () => <div>Note Page for each Id</div>
+
+export default Page;
---
+++ pages/notes/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+// By putting our component in the route pages/notes/index.jsx
+// Next will automatically set the rouyte to be 'http://localhost:3000/notes'
+const Page = () => <div>Notes Page</div>
+
+export default Page;
