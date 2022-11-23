5d42aba68600973bf3920fa24f00fc38ab39253d
mikemtzp <mikemp8226@gmail.com>
2022-11-23 17:59:41 -0600

    Add dynamic route parameters

--- pages/notes/[id].jsx
+++ pages/notes/[id].jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import { useRouter } from 'next/router';
+// use { withRouter } when working with class components
 
 // By putting our component in the route pages/notes/[id].jsx
-// Next will automatically set the rouyte to be 'http://localhost:3000/notes/:id'
-const Page = () => <div>Note Page for each Id</div>
+// Next will automatically set the route to be 'http://localhost:3000/notes/:id'
+
+const Page = () => {
+  // With useRouter we are able to use the query paramas
+  const router = useRouter();
+
+  // router.query is gonna be an object with any associated params on there
+  // We are destructuring { id } because that's the name we gave to the [id].jsx file
+  const { id } = router.query
+
+  // The next snippet is going to display the id that we pass to the route in the 'h1' title
+  return (
+    <h1>
+      Note {id}
+    </h1>
+  )
+}
 
 export default Page;
