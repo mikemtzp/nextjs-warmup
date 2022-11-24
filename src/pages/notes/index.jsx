86c66d58c018bd409b0efafd7f44e20aec9a6bd2
mikemtzp <mikemp8226@gmail.com>
2022-11-24 08:08:00 -0600

    Add page navigation with Link and push

--- src/pages/notes/index.jsx
+++ src/pages/notes/index.jsx
@@ -1,7 +1,49 @@
 import React from 'react';
+// We use <Link> to navigate between pages using client-side routing
+// Excellent choice for SPA's fast navigation. FOr external links it's better an <a> to avoid re-rendering
+import Link from 'next/link';
 
 // By putting our component in the route pages/notes/index.jsx
 // Next will automatically set the rouyte to be 'http://localhost:3000/notes'
-const Page = () => <div>Notes Page</div>
+const Page = () => {
+  const notes = new Array(15)
+    .fill(1)
+    .map((e, i) => ({ id: i, title: `Note: ${i}` }));
+
+  return (
+    <div>
+      <h1>Notes Page</h1>
+
+      {/* For dynamic routes navigation we pass to href the path of the page
+        rather than an URL and we use the 'as' to pass an ID e.g. as={`notes/${id}`}*/}
+      <p>
+        <Link href='notes/[id]' as={`notes/1`}>
+          Note 1
+        </Link>
+      </p>
+
+      <p>
+        <Link
+          href='notes/[programmatic-routing]'
+          as={`notes/programmatic-routing`}
+        >
+          Got to Programmatic Routing
+        </Link>
+      </p>
+
+      <p>
+        <Link href='/'>Got to Home</Link>
+      </p>
+
+      {notes.map((note) => (
+        <div key={note.id}>
+          <Link key={note.id} href='/notes/[id]' as={`/notes/${note.id}`}>
+            <strong>{note.title}</strong>
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default Page;
